0046a21c875ce08d5a4811cd2ec6555f25b2ce20
wangxiaojust <wangxiao@zrworldwide.com>
2014-10-29 14:25:25 +0800

    登录或注册成功进入欢迎界面

---
+++ app/src/main/java/com/example/xiaoxiao/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.xiaoxiao.myapplication;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+import com.example.xiaoxiao.myapplication.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
